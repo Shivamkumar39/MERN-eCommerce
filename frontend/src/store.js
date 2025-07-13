199c2d09c661d20d907b52cafeae11a807b4319e
Shivamkumar098798 <shivamkumar098798@gmail.com>
2025-07-13 16:13:38 +0530

    commit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,20 +5,19 @@ import cartSliceReducer from './slices/cartSlice';
 import authSliceReducer from './slices/authSlice';
 import searchProductSliceReducer from './slices/searchProductSlice';
 
-// Configure the Redux store
+
 const store = configureStore({
-  // Combine reducers for different slices
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer, // API-related state reducer
-    cart: cartSliceReducer, // Shopping cart state reducer
-    auth: authSliceReducer, // Authentication state reducer
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer, 
+    auth: authSliceReducer, 
     search: searchProductSliceReducer
   },
 
-  // Add middleware to the Redux store
+
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(apiSlice.middleware) // Add API middleware
+    getDefaultMiddleware().concat(apiSlice.middleware)
 });
 
-// Export the configured Redux store for use in the application
+
 export default store;
